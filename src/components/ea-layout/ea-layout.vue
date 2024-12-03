60202d557a2581420e67ef844609f3f2afe9f246
Dustin Heisey <dustin@heisey.email>
2024-12-03 13:55:58 +0100

    feat: start layout component

---
+++ src/components/ea-layout/ea-layout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Logo from '../../assets/img/logo.svg'
+</script>
+
+<template>
+  <header class="inset-xl">
+    <a href="/" class="cluster gap-xs">
+      <Logo class="logo" />
+      <h2 class="color-primary">Evergreen Academy</h2>
+    </a>
+  </header>
+  <main><slot></slot></main>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  inline-size: 7rem;
+  block-size: auto;
+  vertical-align: middle;
+}
+h1 {
+  line-height: 1;
+  display: inline-block;
+}
+</style>
