2418954ce6cd14f76119833a03dd588ef5732939
Dustin Heisey <dustin@heisey.email>
2024-12-03 20:29:44 +0100

    feat: add basic profile text component

---
+++ src/components/ea-profile-text/ea-profile-text.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  year: string
+  sport: string
+  team: string
+  intro: string
+  description: string
+}>()
+</script>
+
+<template>
+  <article class="prose">
+    <h1 class="region-headline color-primary">{{ name }}</h1>
+    <div class="cluster gap-xs">
+      <div class="stack">
+        <p class="overline">Year</p>
+        <p class="color-primary">{{ year }}</p>
+      </div>
+      <div class="stack">
+        <p class="overline">Sport</p>
+        <p class="color-primary">{{ sport }}</p>
+      </div>
+      <div class="stack">
+        <p class="overline">Team</p>
+        <p class="color-primary">{{ team }}</p>
+      </div>
+    </div>
+    <p>
+      {{ intro }}
+    </p>
+    <p>
+      {{ description }}
+    </p>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.overline {
+  color: var(--color-on-surface);
+}
+
+p.color-primary {
+  min-inline-size: unset;
+}
+</style>
