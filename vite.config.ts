8778f1455cc1fc3d55aa162c9bbaea371d0ca2c6
Dustin Heisey <dustin@heisey.email>
2024-12-04 19:00:19 +0100

    refactor: add some aliases

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      components: fileURLToPath(new URL('./src/components/index.ts', import.meta.url)),
     },
   },
 })
