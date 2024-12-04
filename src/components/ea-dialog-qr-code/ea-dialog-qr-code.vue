1df465e942af2b3f46eafdd0c661d7d405ddee40
Dustin Heisey <dustin@heisey.email>
2024-12-04 09:57:57 +0100

    feat: qr code dialog component

---
+++ src/components/ea-dialog-qr-code/ea-dialog-qr-code.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import EaIcon from '../ea-icon/ea-icon.vue'
+import QRCode from '../../assets/img/qr-code.png'
+import EaDialog from '../ea-dialog/ea-dialog.vue'
+</script>
+
+<template>
+  <ea-dialog btnVariant="primary" icon="qr-code" btnLabel="QR Code" title="QR Code">
+    <div class="stack gap-xs inset-m align-center">
+      <ea-icon name="camera" size="xl"></ea-icon>
+      <p class="caption space-xs">Scan with your Camera App to view the app on your phone.</p>
+      <img :src="QRCode" alt="QR Code" />
+    </div>
+  </ea-dialog>
+</template>
+
+<style scoped lang="scss">
+img {
+  inline-size: 50%;
+}
+</style>
--- src/components/ea-dialog/ea-dialog.vue
+++ src/components/ea-dialog/ea-dialog.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import EaBtn from '../ea-btn/ea-btn.vue'
+import type { Icons } from '../component-types.ts'
 
 const modal = ref<HTMLDialogElement | null>(null)
 
-defineProps<{
+const {
+  title,
+  btnVariant = 'icon',
+  btnLabel,
+} = defineProps<{
   title: string
+  btnVariant?: 'primary' | 'secondary' | 'tertiary' | 'icon' | 'icon-subtle'
+  icon?: Icons
+  btnLabel?: string
 }>()
 
 const showModal = (): void => {
@@ -22,55 +30,36 @@ const closeModal = (): void => {
 </script>
 
 <template>
-  <ea-btn variant="icon" icon="accessibility" @click="showModal"></ea-btn>
-  <dialog class="modal" @close="closeModal" ref="modal" data-modal>
-    <div class="modal-header">
-      <h2>{{ title }}</h2>
-      <ea-btn variant="icon-subtle" icon="close" @click="closeModal" aria-label="Close"></ea-btn>
-    </div>
-    <div class="modal-body">
+  <ea-btn :variant="btnVariant" :icon="icon" @click="showModal" :aria-label="title">{{
+    btnLabel
+  }}</ea-btn>
+  <dialog class="modal inset-s" @close="closeModal" ref="modal" data-modal>
+    <section class="stack gap-2xs">
+      <div class="cluster justify-between">
+        <h2 class="color-primary">{{ title }}</h2>
+        <ea-btn variant="icon-subtle" icon="close" @click="closeModal" aria-label="Close"></ea-btn>
+      </div>
+      <hr />
       <slot></slot>
-    </div>
+    </section>
   </dialog>
 </template>
 
 <style scoped>
 .modal {
-  padding: 1rem;
   border: none;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: var(--radius-m);
+  box-shadow: var(--shadow-xl);
+  background-color: var(--color-surface-container-high);
   max-width: 500px;
   width: 90%;
 }
 
-.modal-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #ddd;
-  padding-bottom: 0.5rem;
+hr {
+  border-block-start: 1px solid #ddd;
 }
-
-.modal-header h2 {
+h2 {
   margin: 0;
   font-size: 1.25rem;
-}
-
-.close-button {
-  background: none;
-  border: none;
-  font-size: 1.5rem;
-  cursor: pointer;
-}
-
-.modal-body {
-  margin-top: 1rem;
-  font-size: 1rem;
-}
-
-.modal-footer {
-  margin-top: 1rem;
-  text-align: right;
 }
 </style>
