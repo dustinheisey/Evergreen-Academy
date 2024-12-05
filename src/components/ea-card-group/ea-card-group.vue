3d75986eb37a3a3c7c8a799b281d6a89ad303b10
Dustin Heisey <dustin@heisey.email>
2024-12-05 09:07:09 +0100

    feat: group card component

---
+++ src/components/ea-card-group/ea-card-group.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+// Props definition
+const props = defineProps<{
+  variant?: 'student' | 'award'
+  imageSrc?: string
+  imageAlt?: string
+  headline?: string
+  subHeadline?: string
+  years?: Array<string>
+  year?: string
+  team?: string
+  activity?: string
+  cardHref?: string
+}>()
+</script>
+
+<template>
+  <component :is="cardHref ? 'router-link' : 'article'" :to="cardHref" class="card">
+    <img v-if="imageSrc" :src="imageSrc" :alt="imageAlt" />
+  </component>
+</template>
+
+<style scoped lang="scss">
+.card {
+  box-shadow: var(--shadow-m);
+  border-radius: var(--radius-l);
+  position: relative;
+  display: inline-block;
+  box-shadow: var(--shadow-m);
+  border-radius: var(--radius-l);
+  overflow: hidden;
+  transition:
+    box-shadow 0.2s ease-in-out,
+    transform 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: var(--shadow-l);
+  }
+}
+
+img {
+  inline-size: 100%;
+}
+
+.card:hover {
+  box-shadow: var(--shadow-l);
+  transform: translateY(-5px);
+  cursor: pointer;
+}
+</style>
--- src/views/grid-groups/grid-groups.vue
+++ src/views/grid-groups/grid-groups.vue
@@ -1,60 +1,58 @@
 <script setup lang="ts">
-import EaCard from '../../components/ea-card/ea-card.vue'
-import Athletes from '../../assets/img/athletes.png'
-import Artists from '../../assets/img/artists.png'
-import CommunityLeaders from '../../assets/img/community-leaders.png'
-import EnvironmentalStewards from '../../assets/img/environmental-stewards.png'
-import Scholars from '../../assets/img/scholars.png'
-import Awards from '../../assets/img/awards.png'
+import { EaCardGroup } from 'components'
+import Athletes from '@assets/img/groups/athletes.png'
+import Artists from '@assets/img/groups/artists.png'
+import CommunityLeaders from '@assets/img/groups/community-leaders.png'
+import EnvironmentalStewards from '@assets/img/groups/environmental-stewards.png'
+import Scholars from '@assets/img/groups/scholars.png'
+import Awards from '@assets/img/groups/awards.png'
+// TODO: layout arrangement, vue router link card, image card template
 
 const {
-  categories = [
+  groups = [
     {
       img: Athletes,
-      category: 'Athletes',
+      group: 'Athletes',
     },
     {
       img: Artists,
-      category: 'Artists',
+      group: 'Artists',
     },
     {
       img: CommunityLeaders,
-      category: 'Community Leaders',
+      group: 'Community Leaders',
     },
     {
       img: EnvironmentalStewards,
-      category: 'Environmental Stewards',
+      group: 'Environmental Stewards',
     },
     {
       img: Scholars,
-      category: 'Scholars',
+      group: 'Scholars',
     },
     {
       img: Awards,
-      category: 'Awards',
+      group: 'Awards',
     },
   ],
 } = defineProps<{
-  categories: [{ img: string; category: string }]
+  groups: [{ img: string; group: string }]
 }>()
 </script>
 
 <template>
-  <Layout>
-    <section class="grid center grid-3">
-      <ea-card
-        v-for="(category, index) in categories"
-        variant="category"
-        :key="index"
-        :imageSrc="category.img"
-        :headline="category.category"
-      />
-    </section>
-  </Layout>
+  <section class="grid center grid-3 gap-m">
+    <ea-card-group
+      v-for="(group, index) in groups"
+      :key="index"
+      :imageSrc="group.img"
+      :headline="group.group"
+    />
+  </section>
 </template>
 
 <style scoped lang="scss">
 .grid {
-  place-items: center;
+  max-inline-size: 1200px;
 }
 </style>
