595b2d7916129a6b96d04067ed3e9917de936a2c
Dustin Heisey <dustin@heisey.email>
2024-12-05 11:16:48 +0100

    feat: layout component

--- src/components/ea-layout/ea-layout.vue
+++ src/components/ea-layout/ea-layout.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import Logo from '@assets/img/logo.svg'
-import { EaBackBtn } from 'components'
-
-const route = useRoute()
-
-const isHome = computed(() => route.name === 'home')
+import { EaBtn, EaBackBtn, EaDialogQrCode, EaDialogAccessibility } from 'components'
 </script>
 
 <template>
-  <header class="inset-xl">
-    <a href="/" class="cluster gap-xs">
-      <Logo class="logo" />
-      <h2 class="color-primary">Evergreen Academy</h2>
-      <ea-back-btn v-if="!isHome"></ea-back-btn>
-    </a>
-  </header>
-  <main class="stack gap-m inset-xl">
-    <slot></slot>
-  </main>
+  <div class="cover gap-s inset-s">
+    <header class="cluster justify-between header inset-0">
+      <RouterLink to="/" class="cluster gap-xs">
+        <Logo class="logo" />
+        <h2 class="color-primary">Evergreen Academy</h2>
+      </RouterLink>
+      <ea-btn variant="icon" icon="search"></ea-btn>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <section class="cluster justify-between btns">
+      <ea-back-btn></ea-back-btn>
+      <div class="cluster">
+        <ea-dialog-qr-code></ea-dialog-qr-code>
+        <ea-dialog-accessibility></ea-dialog-accessibility>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.cover {
+  block-size: 100vh;
+}
 .logo {
   inline-size: 7rem;
   block-size: auto;
@@ -31,5 +38,10 @@ const isHome = computed(() => route.name === 'home')
 h1 {
   line-height: 1;
   display: inline-block;
+}
+
+main {
+  overflow-y: scroll;
+  max-block-size: 100vh;
 }
 </style>
