29ce432185cc3e0d03048cae8199787ae279fc51
Dustin Heisey <dustin@heisey.email>
2024-12-06 18:27:34 +0100

    fix: show profile images again

--- src/views/profile-simple/profile-simple.vue
+++ src/views/profile-simple/profile-simple.vue
@@ -2,14 +2,22 @@
 import { EaLayout, EaProfileText } from '@/components'
 import type { Student } from '@/types'
 
+const studentImages = import.meta.glob('@/assets/img/students/*.jpg', {
+  eager: true,
+  query: '?url',
+  import: 'default',
+})
+
 const props = defineProps<Student>()
+
+const src = studentImages[`/src/assets/img/students/${props.slug}.jpg`] as string
 </script>
 
 <template>
   <ea-layout>
     <section class="switcher center switch-phone gap-m">
       <div class="frame square fixed">
-        <img :src="`/img/students/${slug}.jpg`" :alt="name" />
+        <img :src :alt="name" />
       </div>
       <ea-profile-text v-bind="props"></ea-profile-text>
     </section>
