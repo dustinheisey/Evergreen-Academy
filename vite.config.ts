0436c7f46ad87aa45dbf0d5c797d531cf239af8b
Dustin Heisey <dustin@heisey.email>
2024-12-06 11:56:29 +0100

    feat: add PWA functionality

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,40 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
+import { VitePWA } from 'vite-plugin-pwa'
+
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  manifest: {
+    name: 'Evergreen Academy',
+    short_name: 'Evergreen',
+    theme_color: '#1a4c23',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: ['any', 'maskable'],
+      },
+    ],
+  },
+}
 
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
